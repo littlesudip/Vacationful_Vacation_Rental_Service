a58579664b01e98279e091311cc2cb2200abe18d
Fahmidah Khan <45175241+FahmidahKhan@users.noreply.github.com>
2020-09-21 17:45:35 +0600

    property model

---
+++ PropertyManagement/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PropertymanagementConfig(AppConfig):
+    name = 'PropertyManagement'
---
+++ PropertyManagement/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Property(models.Model):
+    property_ID=models.IntegerField(blank=True,null=True)
+    property_name= models.CharField(max_length=200)
+    property_location=models.CharField(max_length=200)
+    property_details=models.CharField(max_length=200)
+    property_pricing= models.FloatField(blank=True,null=True)
