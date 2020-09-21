f7a7075604846ddca2a9f380e15d0895142dd04d
Fahmidah Khan <45175241+FahmidahKhan@users.noreply.github.com>
2020-09-21 12:46:58 +0600

    Update models.py

--- UserManagement/models.py
+++ UserManagement/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     user_type = models.CharField(max_length=100)
-    NID_ID = models.IntegerField(blank=True,null=True)
+    nid_id = models.IntegerField(blank=True,null=True)
     name=models.CharField(max_length=100)
     email=models.EmailField(max_length=200)
     address=models.CharField(max_length=300)
