59dfab5d09827cc8db7b4dd80f0e959ca850bfbf
Fahmidah Khan <45175241+FahmidahKhan@users.noreply.github.com>
2020-09-21 12:45:09 +0600

    user management

--- UserManagement/models.py
+++ UserManagement/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_id=models.IntegerField()
-    use_type=models.CharField(max_length=100)
-    nid_id=models.IntegerField(blank=True,null=True)
+    user_type = models.CharField(max_length=100)
+    NID_ID = models.IntegerField(blank=True,null=True)
     name=models.CharField(max_length=100)
     email=models.EmailField(max_length=200)
     address=models.CharField(max_length=300)
