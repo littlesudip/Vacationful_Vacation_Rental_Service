55f011121c89ab2c4f5a50f283f66900c422b4de
Anushka Azad <68438635+Anushkaazad@users.noreply.github.com>
2020-09-21 16:38:03 +0600

    changes model

--- UserManagement/models.py
+++ UserManagement/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Address(models.Model):
-    house_no = models.IntegerField(blank=True,null=True,default="")
-    street_address = models.CharField(max_length= 200)
-    postal_address = models.CharField(max_length = 200)
-    post_code = models.CharField(max_length=20)
-    city = models.CharField(max_length = 20)
 
-def __str__(self):
-    return str(self.house_no)+""+self.city
+
+
 
 class User(models.Model):
     user_type = models.CharField(max_length=100)
@@ -20,8 +14,7 @@ class User(models.Model):
     contact_no=models.IntegerField(blank=True,null=True)
 
 
-def __str__(self):
-    return self.name
+
 
 
 
