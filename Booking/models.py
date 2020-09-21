f0775a5ca4eeba9d38821f79e0129c07b3fa3d82
Ankita Paul <68142228+AnkitaPaul091@users.noreply.github.com>
2020-09-21 20:08:08 +0600

    Updated Booking_System

--- Booking/models.py
+++ Booking/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db imports models
+from PropertyManagement import Property
+from Traveller import User_ID
 
 # Create your models here.
+
+class BookingManagement(models.Model):
+	Property_Info = models.ForeignKey(Property, on_delete=models.CASCADE, default=1, null=True)
+	Info = models.ForeignKey(Traveller, on_delete=models.CASCADE, default=1, null=True)
+
