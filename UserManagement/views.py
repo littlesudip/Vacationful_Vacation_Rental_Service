1b37b437127a3b035747a3930259bcd67cfe6090
Ankita Paul <68142228+AnkitaPaul091@users.noreply.github.com>
2020-09-23 01:57:58 +0600

    modified

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .forms import forms
 from .models import User
+from django.shortcuts import render
+
 # Create your views here.
+from UserManagement.forms import infoForm
 
 def showuserinfo(request):
 
@@ -11,17 +14,25 @@ def showuserinfo(request):
                }
     return render(request,"UserManagement/showuserinfo.html",context)
 
-def insertinfo(request):
-    form = forms()
 
+
+def inserinfo(request):
+
+    message = ""
+    form= infoForm()
+    if request.method == "POST":
+
+        form = infoForm(request.POST)
+    message = "Invalid input. Please try again!"
+    if form.is_valid():
+        form.save()
+        message = "Thank You"
+        form = infoForm()
     context = {
-            'form' : form
+        'form': form,
+        'message' : message
     }
-    return render(request, "UserManagement/insertinfo.html", context)
-
-
-
-
+    return render(request, 'User/inserinfo.html' ,context)
 
 
 
