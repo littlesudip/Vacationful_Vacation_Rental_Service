63578005f696c3f1557e85a2c369a745ae8fc26c
Sudip Ghose <littlesudip007@gmail.com>
2020-09-26 20:09:00 +0600

    New_Project

--- Vacation/asgi.py
+++ Vacation/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Vacationful project.
+ASGI config for Vacation project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vacationful.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vacation.settings')
 
 application = get_asgi_application()
