86eb181bc4362139e534c446c41ef935c16c9253
Ankita Paul <68142228+AnkitaPaul091@users.noreply.github.com>
2020-09-29 07:25:31 +0600

    Owner

---
+++ Owner/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class OwnerConfig(AppConfig):
+    name = 'Owner'
--- Vacationful/settings.py
+++ Vacationful/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'UserManagement.apps.UsermanagementConfig',
-    'Traveller.apps.TravellerConfig'
+    'Traveller.apps.TravellerConfig',
+    'Owner.apps.OwnerConfig',
 
 ]
 
