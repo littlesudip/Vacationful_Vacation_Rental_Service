89ac8237b24fe6dda54441bf864bf2b7bfdb9bca
Ankita Paul <68142228+AnkitaPaul091@users.noreply.github.com>
2020-09-29 15:37:13 +0600

    modified

--- Booking/models.py
+++ Booking/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from Traveller.models import Traveller
 
+
 # Create your models here.
 
 class Booking(models.Model):
@@ -8,10 +9,12 @@ class Booking(models.Model):
     Checkin_Time = models.CharField(max_length=100)
     Checkout_Date = models.CharField(max_length=100)
     Checkout_time = models.CharField(max_length=100)
-    No_of_Guests = models.IntegerField(max_length=100)
+    No_of_Guests = models.IntegerField(blank=True, null=True)
     traveller_Info = models.ForeignKey(Traveller,on_delete=models.CASCADE)
+
 
 def __str__(self):
         return self.traveller_Info.tarvelling_area
+
 
 
